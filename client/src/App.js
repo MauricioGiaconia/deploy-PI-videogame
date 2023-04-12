3f71e9b3e759bea4c354255190da1b9572b114b5
MauricioGiaconia <mauriciogiaco@gmail.com>
2023-04-12 20:34:27 -0300

    CHANGE: Deleted useEffect from the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,29 +6,15 @@ import Home from './components/Home/Home';
 import NewGameForm from './components/NewGameForm/NewGameForm';
 import Detail from './components/Detail/Detail';
 import Footer from './components/Footer/Footer';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getGenres } from './redux/actions';
 import axios from 'axios';  
-axios.defaults.baseURL = 'https://deploy-pi-videogame-production.up.railway.app/';
+axios.defaults.baseURL = 'http://localhost:3001/';
 
 function App() {
-  const genres = useSelector(state => state.genres);
-  const dispatch = useDispatch();
-
-  //Revisar porque traigo todos los generos
-  useEffect(() => {
-      if (genres.length <= 0){
-          dispatch(getGenres());
-      }
-  }, []);
+  
 
   return (
     <div className="App">
       <Nav></Nav>
-
-
-
 
         <Routes>
           <Route path='/' element={<Home/>} />
