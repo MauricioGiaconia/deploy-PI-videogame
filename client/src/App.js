378d6eccf5b9348fe24e1274ba4ec6be7d7ce37f
MauricioGiaconia <mauriciogiaco@gmail.com>
2023-04-13 19:50:44 -0300

    FIX: Fixed bug with filters

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,19 @@ import NewGameForm from './components/NewGameForm/NewGameForm';
 import Detail from './components/Detail/Detail';
 import Footer from './components/Footer/Footer';
 import axios from 'axios';  
-axios.defaults.baseURL = 'https://deploy-pi-videogame-production.up.railway.app/';
+import { useEffect } from 'react';
+import { getGenres } from './redux/actions';
+import { useDispatch } from 'react-redux';
+
+axios.defaults.baseURL = 'http://localhost:3001/';
 
 function App() {
   
+  const dispatch = useDispatch();
+
+  useEffect(() =>{
+    dispatch(getGenres());
+  }, [])
 
   return (
     <div className="App">
